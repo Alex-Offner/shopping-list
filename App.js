910aefa70af6277c81e39f313c94049152118295
Alex-Offner <80695644+Alex-Offner@users.noreply.github.com>
2021-10-11 18:48:51 -0700

    Add firebase data

--- App.js
+++ App.js
@@ -1,21 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Button, FlatList } from 'react-native';
+import firebase from 'firebase';
+import firestore from 'firebase';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// const firebase = require('firebase');
+// require('firebase/firestore');
+// require('firebase/auth')
+
+
+
+// Careful, this is just an example, you need to implement the same ideas here in Chat.js then import Chat component to App.js and use it there.
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      lists: [],
+      uid: 0,
+      loggedInText: 'Please wait, you are getting logged in',
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        firebaseConfig
+      });
+
+      const firebaseConfig = {
+        apiKey: "AIzaSyA9BlybtbAeWWYmZLV6zagLf1ydaNyhiX4",
+        authDomain: "test-e4daa.firebaseapp.com",
+        projectId: "test-e4daa",
+        storageBucket: "test-e4daa.appspot.com",
+        messagingSenderId: "772187148804",
+        appId: "1:772187148804:web:6a61d892ad4ea8c8fa1b51",
+        measurementId: "G-2QTL1F2KBS"
+      };
+    }
+  }
+
+  componentDidMount() {
+    this.referenceShoppingLists = firebase.firestore().collection('shoppinglists');
+    this.unsubscribe = this.referenceShoppingLists.onSnapshot(this.onCollectionUpdate)
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const lists = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      var data = doc.data();
+      lists.push({
+        name: data.name,
+        items: data.items.toString(),
+      });
+    });
+    this.setState({
+      lists,
+    });
+  };
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.lists}
+          renderItem={({ item }) =>
+            <Text style={styles.item}>{item.name}: {item.items}</Text>}
+        />
+
+        <Button
+          onPress={() => {
+            this.addList();
+          }}
+          title="Add something"
+        />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40,
   },
+  item: {
+    fontSize: 20,
+    color: 'blue',
+  },
+  text: {
+    fontSize: 30,
+  }
 });
+
+export default App;
